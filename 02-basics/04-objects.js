b5e2f514814ca77d64a19b3448346e8a6d77cd32
Faizal Ahmad <affa17082022@gmail.com>
2025-06-05 13:15:04 +0000

    objects

--- 02-basics/04-objects.js
+++ 02-basics/04-objects.js
@@ -17,7 +17,7 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.firstname);
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
@@ -46,11 +46,31 @@ const users =[
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+const course = {
+    coursename: "js in hindi",
+    price: "000",
+    courseInstructor: "fizull"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+{
+    "name": "faizal",
+    "coursename": "jsfreee",
+    "price": "free"
+}
+
